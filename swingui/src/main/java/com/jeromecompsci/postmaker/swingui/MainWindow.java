3c29b255c5bade40187746f465dfcf4ac9921938
wangdderek <wangdderek@outlook.com>
2015-11-02 20:21:00 -0500

    [swingui] Accomodate ActionPanel in the MainWindow.

--- swingui/src/main/java/com/jeromecompsci/postmaker/swingui/MainWindow.java
+++ swingui/src/main/java/com/jeromecompsci/postmaker/swingui/MainWindow.java
@@ -16,11 +16,11 @@ public class MainWindow extends JFrame {
     }
 
     InputPanel inputPanel;
-    JButton postButton;
+    ActionPanel actionPanel;
 
     public void initComponents() {
         inputPanel = new InputPanel();
-        postButton = new JButton("Post");
+        actionPanel = new ActionPanel();
     }
 
     public void initLayout() {
@@ -29,9 +29,10 @@ public class MainWindow extends JFrame {
         contentPane.setLayout(layout);
 
         this.add(inputPanel, BorderLayout.CENTER);
+        this.add(actionPanel, BorderLayout.LINE_END);
 
         this.pack();
-        this.setMinimumSize(new Dimension(800, 700));
+        this.setMinimumSize(new Dimension(1200, 900));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("PostMaker");
     }
