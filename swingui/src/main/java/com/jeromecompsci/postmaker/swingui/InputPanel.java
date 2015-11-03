95a92b108fa6c8cf0bbf3c2e7533ceeec55379ee
wangdderek <wangdderek@outlook.com>
2015-11-02 20:00:12 -0500

    [swingui] Reasonably behaved InputPanel based on SwingLayout (BoxLayout never works)

--- swingui/src/main/java/com/jeromecompsci/postmaker/swingui/InputPanel.java
+++ swingui/src/main/java/com/jeromecompsci/postmaker/swingui/InputPanel.java
@@ -24,24 +24,77 @@ public class InputPanel extends JPanel {
         categoriesField = new JTextField();
     }
 
+    static final String NORTH = SpringLayout.NORTH;
+    static final String SOUTH = SpringLayout.SOUTH;
+    static final String EAST = SpringLayout.EAST;
+    static final String WEST = SpringLayout.WEST;
+
+    static final int B = 12; // Border (margin from edges)
+    static final int S = 4; // Spacing (between components)
+
     public void initLayout() {
-        BoxLayout layout = new BoxLayout(this, BoxLayout.PAGE_AXIS);
+        SpringLayout layout = new SpringLayout();
         this.setLayout(layout);
 
         JLabel titleLabel = new JLabel("Title");
-        titleLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JLabel fullTextLabel = new JLabel("Content");
+        JLabel blurbLabel = new JLabel("Blurb");
+        JLabel categoriesLabel = new JLabel("Categories");
+
         this.add(titleLabel);
-        titleField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+        this.add(fullTextLabel);
+        this.add(blurbLabel);
+        this.add(categoriesLabel);
+
         this.add(titleField);
-        this.add(new JLabel("Content"));
-        fullTextArea.setPreferredSize(new Dimension(400, 150));
         this.add(fullTextArea);
-        this.add(new JLabel("Blurb"));
-        blurbTextArea.setPreferredSize(new Dimension(400, 120));
-        blurbTextArea.setMaximumSize(new Dimension(Integer.MAX_VALUE, 120));
         this.add(blurbTextArea);
-        this.add(new JLabel("Categories"));
-        categoriesField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
         this.add(categoriesField);
+
+        titleField.setPreferredSize(new Dimension(100, 60));
+        titleField.setMaximumSize(titleField.getPreferredSize());
+//        fullTextArea.setPreferredSize(new Dimension(400, 150));
+//        blurbTextArea.setPreferredSize(new Dimension(400, 120));
+        blurbTextArea.setPreferredSize(new Dimension(100, 120));
+        blurbTextArea.setMaximumSize(blurbTextArea.getPreferredSize());
+        categoriesField.setPreferredSize(new Dimension(100, 60));
+        categoriesField.setMaximumSize(categoriesField.getPreferredSize());
+
+        layout.putConstraint(NORTH, titleLabel, B, NORTH, this);
+        layout.putConstraint(NORTH, titleField, S, SOUTH, titleLabel);
+        layout.putConstraint(NORTH, fullTextLabel, S, SOUTH, titleField);
+        layout.putConstraint(NORTH, fullTextArea, S, SOUTH, fullTextLabel);
+        layout.putConstraint(NORTH, blurbLabel, S, SOUTH, fullTextArea);
+        layout.putConstraint(NORTH, blurbTextArea, S, SOUTH, blurbLabel);
+        layout.putConstraint(NORTH, categoriesLabel, S, SOUTH, blurbTextArea);
+        layout.putConstraint(NORTH, categoriesField, S, SOUTH, categoriesLabel);
+
+        System.out.println("blurbTextArea = " + blurbTextArea.getHeight());
+        layout.putConstraint(SOUTH, this,
+                (int) (4*S + B
+                + blurbLabel.getPreferredSize().getHeight()
+                + blurbTextArea.getPreferredSize().getHeight()
+                + categoriesLabel.getPreferredSize().getHeight()
+                + categoriesField.getPreferredSize().getHeight()),
+                SOUTH, fullTextArea);
+
+        layout.putConstraint(WEST, titleLabel, B, WEST, this);
+        layout.putConstraint(WEST, titleField, B, WEST, this);
+        layout.putConstraint(WEST, fullTextLabel, B, WEST, this);
+        layout.putConstraint(WEST, fullTextArea, B, WEST, this);
+        layout.putConstraint(WEST, blurbLabel, B, WEST, this);
+        layout.putConstraint(WEST, blurbTextArea, B, WEST, this);
+        layout.putConstraint(WEST, categoriesLabel, B, WEST, this);
+        layout.putConstraint(WEST, categoriesField, B, WEST, this);
+
+        layout.putConstraint(EAST, titleLabel, -B, EAST, this);
+        layout.putConstraint(EAST, titleField, -B, EAST, this);
+        layout.putConstraint(EAST, fullTextLabel, -B, EAST, this);
+        layout.putConstraint(EAST, fullTextArea, -B, EAST, this);
+        layout.putConstraint(EAST, blurbLabel, -B, EAST, this);
+        layout.putConstraint(EAST, blurbTextArea, -B, EAST, this);
+        layout.putConstraint(EAST, categoriesLabel, -B, EAST, this);
+        layout.putConstraint(EAST, categoriesField, -B, EAST, this);
+
     }
 }
