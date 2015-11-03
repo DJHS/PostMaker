c7d0acb9ba8ac723a67f7c51aeacdda043693a4f
wangdderek <wangdderek@outlook.com>
2015-11-02 19:14:31 -0500

    [swingui] Start on a MainWindow.

---
+++ swingui/src/main/java/com/jeromecompsci/postmaker/swingui/MainWindow.java
@@ -0,0 +1,51 @@
+package com.jeromecompsci.postmaker.swingui;
+
+import javax.swing.*;
+import com.jeromecompsci.postmaker.core.PostModel;
+
+import java.awt.*;
+
+/**
+ * @author derek
+ */
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        super();
+        initComponents();
+        initLayout();
+    }
+
+    InputPanel inputPanel;
+    JButton postButton;
+
+    public void initComponents() {
+        inputPanel = new InputPanel();
+        postButton = new JButton("Post");
+    }
+
+    public void initLayout() {
+        Container contentPane = this.getContentPane();
+        BorderLayout layout = new BorderLayout();
+        contentPane.setLayout(layout);
+
+        this.add(inputPanel, BorderLayout.CENTER);
+
+        this.pack();
+        this.setMinimumSize(new Dimension(600, 500));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("PostMaker");
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e) {
+                    // Do Nothing (it will fall back to default LnF)
+                }
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+}
