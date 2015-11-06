d81aba03ce7bdefcace58ebd09060e29c8a29eea
wangdderek <wangdderek@outlook.com>
2015-11-05 23:11:37 -0500

    Make Publishers have an easy job to send out updates using a publishinglistener

--- core/src/main/java/com/jeromecompsci/postmaker/core/Publisher.java
+++ core/src/main/java/com/jeromecompsci/postmaker/core/Publisher.java
@@ -5,6 +5,38 @@ import java.util.Properties;
 /**
  * @author derek
  */
-public interface Publisher {
-    void publish(PostModel post);
+public abstract class Publisher {
+    public abstract void publish(PostModel post);
+
+    private PublishingListener publishingListener;
+
+    public void registerPublishingListener(PublishingListener listener) {
+        this.publishingListener = listener;
+    }
+
+    protected void initProgress(int max) {
+        if (publishingListener != null)
+            publishingListener.progressInitialized(max);
+    }
+
+    protected void updateProgress(int progress) {
+        if (publishingListener != null)
+            publishingListener.progressUpdated(progress);
+    }
+
+    protected void updateInfo(String info) {
+        if (publishingListener != null)
+            publishingListener.infoUpdated(info);
+    }
+
+    protected void declareDone() {
+        if (publishingListener != null)
+            publishingListener.done();
+    }
+
+    protected void throwException(Exception e) {
+        if (publishingListener != null)
+            publishingListener.exceptionThrown(e);
+    }
+
 }
